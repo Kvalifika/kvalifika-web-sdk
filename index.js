4bc58ead790e099796ffff8b45db10dcf5eb46fb
saba-coding <saba.chikhina@gmail.com>
2020-02-10 16:30:45 +0400

    chore: move host to env file

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/check-session/:sessionId', (req, res) => {
   // in options we need to pass sessionId as parameter
   // and our secret token as authorization header
   const options = {
-    url: `https://apidev.kyc.ge/verification/status/${sessionId}`,
+    url: `${process.env.KYC_API_HOST}/verification/status/${sessionId}`,
     headers: { Authorization }
   };
 
