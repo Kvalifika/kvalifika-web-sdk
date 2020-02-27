26c6ddfb4345d6c2c8480e9befec72e55cbca10b
saba-coding <saba.chikhina@gmail.com>
2020-02-27 15:37:13 +0400

    chore: update routes

--- index.js
+++ index.js
@@ -15,6 +15,23 @@ app.use(express.static('public'))
 // This is only for demo purposes store this token inside enviroment variables and do not show it to anyone
 const Authorization = process.env.SECRET_TOKEN
 
+const requestClose = (sessionId) => {
+  const closeOptions = {
+    url: `${process.env.KYC_API_HOST}/verification/close/${sessionId}`,
+    method: "POST",
+    headers: { Authorization }
+  };
+
+  request(closeOptions, (err, response, body) => {
+    console.log("TCL: requestClose -> body", body)
+    if(err) {
+      // handle error
+    } else {
+      // handle success
+    }
+  })
+}
+
 // Here we register rest route to check users session status
 app.get('/check-session/:sessionId', (req, res) => {
   const sessionId = req.params.sessionId
@@ -22,7 +39,7 @@ app.get('/check-session/:sessionId', (req, res) => {
   // in options we need to pass sessionId as parameter
   // and our secret token as authorization header
   const options = {
-    url: `${process.env.KYC_API_HOST}/verification/status/${sessionId}`,
+    url: `${process.env.KYC_API_HOST}/verification/session-data/${sessionId}`,
     headers: { Authorization }
   };
 
@@ -33,6 +50,7 @@ app.get('/check-session/:sessionId', (req, res) => {
       res.status(400).json(err)
     } else {
       // handle success
+      requestClose(sessionId)
       res.status(200).json(body)
     }
   })
